188fbd621302c1eea43df94e7c03feeb1e12c790
caianmei <1610143991@qq.com>
2019-08-23 16:11:08 +0800

    代码格式

--- src/test/java/com/thoughtworks/tdd/FizzBuzzNumberGameTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzNumberGameTest.java
@@ -7,97 +7,97 @@ public class FizzBuzzNumberGameTest {
 
 	@Test
 	public void should_return_1_when_fizzBuzz_Then_1() {
-		//Given
-		
+		// Given
+
 		int expected = 1;
 		FizzBuzzNumberGame fizzBuzzNumberGame = new FizzBuzzNumberGame();
-		//When
+		// When
 		String word = fizzBuzzNumberGame.fizzBuzz(expected);
-		//Then
-		Assertions.assertEquals("1",word);
+		// Then
+		Assertions.assertEquals("1", word);
 	}
-	
+
 	@Test
 	public void should_return_Fizz_when_fizzBuzz_Then_3() {
-		//Given
-		
+		// Given
+
 		int expected = 3;
 		FizzBuzzNumberGame fizzBuzzNumberGame = new FizzBuzzNumberGame();
-		//When
+		// When
 		String word = fizzBuzzNumberGame.fizzBuzz(expected);
-		//Then
-		Assertions.assertEquals("Fizz",word);
+		// Then
+		Assertions.assertEquals("Fizz", word);
 	}
-	
+
 	@Test
 	public void should_return_Buzz_when_fizzBuzz_Then_5() {
-		//Given
-		
+		// Given
+
 		int expected = 5;
 		FizzBuzzNumberGame fizzBuzzNumberGame = new FizzBuzzNumberGame();
-		//When
+		// When
 		String word = fizzBuzzNumberGame.fizzBuzz(expected);
-		//Then
-		Assertions.assertEquals("Buzz",word);
+		// Then
+		Assertions.assertEquals("Buzz", word);
 	}
-	
+
 	@Test
 	public void should_return_Whizz_when_fizzBuzz_Then_7() {
-		//Given
-		
+		// Given
+
 		int expected = 7;
 		FizzBuzzNumberGame fizzBuzzNumberGame = new FizzBuzzNumberGame();
-		//When
+		// When
 		String word = fizzBuzzNumberGame.fizzBuzz(expected);
-		//Then
-		Assertions.assertEquals("Whizz",word);
+		// Then
+		Assertions.assertEquals("Whizz", word);
 	}
-	
+
 	@Test
 	public void should_return_FizzBuzz_when_fizzBuzz_Then_15() {
-		//Given
-		
+		// Given
+
 		int expected = 15;
 		FizzBuzzNumberGame fizzBuzzNumberGame = new FizzBuzzNumberGame();
-		//When
+		// When
 		String word = fizzBuzzNumberGame.fizzBuzz(expected);
-		//Then
-		Assertions.assertEquals("FizzBuzz",word);
+		// Then
+		Assertions.assertEquals("FizzBuzz", word);
 	}
-	
+
 	@Test
 	public void should_return_FizzBuzz_when_fizzBuzz_Then_21() {
-		//Given
-		
+		// Given
+
 		int expected = 21;
 		FizzBuzzNumberGame fizzBuzzNumberGame = new FizzBuzzNumberGame();
-		//When
+		// When
 		String word = fizzBuzzNumberGame.fizzBuzz(expected);
-		//Then
-		Assertions.assertEquals("FizzBuzz",word);
+		// Then
+		Assertions.assertEquals("FizzBuzz", word);
 	}
-	
+
 	@Test
 	public void should_return_FizzBuzz_when_fizzBuzz_Then_35() {
-		//Given
-		
+		// Given
+
 		int expected = 35;
 		FizzBuzzNumberGame fizzBuzzNumberGame = new FizzBuzzNumberGame();
-		//When
+		// When
 		String word = fizzBuzzNumberGame.fizzBuzz(expected);
-		//Then
-		Assertions.assertEquals("FizzBuzz",word);
+		// Then
+		Assertions.assertEquals("FizzBuzz", word);
 	}
-	
+
 	@Test
 	public void should_return_FizzBuzz_when_fizzBuzz_Then_105() {
-		//Given
-		
+		// Given
+
 		int expected = 105;
 		FizzBuzzNumberGame fizzBuzzNumberGame = new FizzBuzzNumberGame();
-		//When
+		// When
 		String word = fizzBuzzNumberGame.fizzBuzz(expected);
-		//Then
-		Assertions.assertEquals("FizzBuzzWhizz",word);
+		// Then
+		Assertions.assertEquals("FizzBuzzWhizz", word);
 	}
 }
